1f36cf58430662884ca03151d2c2c8ca78662af8
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2022-12-06 21:12:02 -0300

    config styledcomponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      Olá DIO
-    </div>
+    <Router>
+      <Routes>   
+        <Route path="/" element={<Home  />} />
+        <Route path="/login" element={<Login  />} />
+      </Routes>
+    </Router>
   );
 }
 
